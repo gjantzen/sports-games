04b873f8411b35c5a086fb75594b80a71bcdfe03
Garth Jantzen <jantzen@gmail.com>
2024-09-07 10:15:55 -0400

    Login debugging

--- src/utils/auth.ts
+++
@@ -1,7 +0,0 @@
-import { timingSafeEqual } from 'crypto';
-
-export function secureCompare(a: string, b: string): boolean {
-  const bufA = Buffer.from(a);
-  const bufB = Buffer.from(b);
-  return bufA.length === bufB.length && timingSafeEqual(bufA, bufB);
-}
