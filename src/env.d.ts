5ff8660c9237af9b6c0dbb05ba80a4f969982caf
Garth Jantzen <jantzen@gmail.com>
2024-09-10 21:09:25 -0400

    Debug

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
 
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {
+    otherLocals: {
+      test: string;
+    };
+  }
+}
+
 interface ImportMetaEnv {
   readonly ADMIN_USERNAME: string;
   readonly ADMIN_PASSWORD: string;
@@ -7,4 +17,9 @@ interface ImportMetaEnv {
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
+}
+
+interface Env {
+  ADMIN_USERNAME: string;
+  ADMIN_PASSWORD: string;
 }
